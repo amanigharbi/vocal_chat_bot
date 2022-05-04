ef67622a7187a01f94ccef67f7cb63ec26588ed4
ameni <amanigharbi9@gmail.com>
2022-05-04 23:29:43 +0100

    Reclamation voicebot v2

--- lib/pages/chatbot.dart
+++ lib/pages/chatbot.dart
@@ -300,33 +300,30 @@ String? type;
               "مرحبا في فضاء الشكايات من فضلك ارسل لي اسم العائلة ",
               MessageType.Receiver,
               DateFormat("HH:mm").format(DateTime.now()));
+               v = 1;
 
           break;
         case 'fr_CA':
           speak(
-              "Bonjour dans l espace de réclamation merci d envoyer votre nom");
+              "Bonjour dans l`espace de réclamation merci d`envoyer votre nom");
           _insertSingleItem(
-              "Bonjour dans l espace de réclamation merci d envoyer votre nom",
+              "Bonjour dans l`espace de réclamation merci d`envoyer votre nom",
               MessageType.Receiver,
               DateFormat("HH:mm").format(DateTime.now()));
           v = 1;
+
           break;
         case 'en_US':
-          speak("Hello! please send me your first name ");
-          _insertSingleItem("Hello! please send me your first name",
+          speak("Hello! please send me your last name ");
+          _insertSingleItem("Hello! please send me your last name",
               MessageType.Receiver, DateFormat("HH:mm").format(DateTime.now()));
-
+ v = 1;
           break;
       }
-
-      print("teeeeeeeeeeeeext " + txt);
       txt = "";
-      print("teeeeeeeeeeeeext2 " + txt);
-      print("vvvv " + v.toString());
     }
     switch (v) {
       case 0:
-        print("ok");
         try {
           var dataForm = {
             "message": txt,
@@ -341,7 +338,6 @@ String? type;
             ),
             data: dataForm,
           );
-          print("aaaaaa " + response.toString());
 
           setState(() async {
             await flutterTts.getLanguages;
@@ -361,69 +357,257 @@ String? type;
       case 1:
         if (txt.toString().isNotEmpty) {
           NomRec = txt;
-          print("la je suis " + txt);
-          NomRec = txt;
           if (NomRec is String && NomRec.toString().trim().length>=4) {
-            speak("Monsieur Madame $NomRec s il vous envoyer moi votre prénom");
+            switch(_currentLocaleId){
+              case 'ar_SA':
+              speak("ارسل لي اسمك $NomRec السيد/السيدة");
             _insertSingleItem(
-                "Monsieur Madame $NomRec s il vous envoyer moi votre prénom",
+                "  ارسل لي اسمك $NomRec السيد/السيدة",
                 MessageType.Receiver,
                 DateFormat("HH:mm").format(DateTime.now()));
+        
+              break;
+              case 'fr_CA':
+              speak("Monsieur/Madame $NomRec s`il vous plait envoyer moi votre prénom");
+            _insertSingleItem(
+                "Monsieur/Madame $NomRec s`il vous plait envoyer moi votre prénom",
+                MessageType.Receiver,
+                DateFormat("HH:mm").format(DateTime.now()));
+            
+              break;
+              case 'en_US':
+                 speak("Mr/Mrs $NomRec please send me your first name");
+            _insertSingleItem(
+                "Mr/Mrs $NomRec please send me your first name",
+                MessageType.Receiver,
+                DateFormat("HH:mm").format(DateTime.now()));
+              break;
+            
+            }
             v = 2;
+          }
+          else{
+              switch (_currentLocaleId) {
+            case 'ar_SA':
+                    speak("اسم غير صحيح");
+          _insertSingleItem("اسم غير صحيح", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+               case 'fr_CA':
+                    speak("Nom non valide");
+          _insertSingleItem("Nom non valide", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+               case 'en_US':
+                    speak("Invalid name");
+          _insertSingleItem("Invalid name", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+              }
           }
         }
         break;
       case 2:
-        print("la je suis prenom " + txt);
         PrenomRec = txt;
         if (PrenomRec is String && PrenomRec.toString().trim().length>3) {
-          speak("Monsieur Madame $NomRec $PrenomRec  envoyer moi votre cin");
+           switch(_currentLocaleId){
+              case 'ar_SA':
+              speak("ارسل لي رقم بطاقة هويتك $PrenomRec $NomRec السيد/السيدة");
+            _insertSingleItem(
+                "  ارسل لي رقم بطاقة هويتك $PrenomRec $NomRec السيد/السيدة",
+                MessageType.Receiver,
+                DateFormat("HH:mm").format(DateTime.now()));
+         
+              break;
+              case 'fr_CA':
+             speak("Monsieur/Madame $NomRec $PrenomRec  envoyer moi votre numéro de carte d`identité");
           _insertSingleItem(
-              "Monsieur Madame $NomRec $PrenomRec  envoyer moi votre cin",
+              "Monsieur/Madame $NomRec $PrenomRec  envoyer moi votre numéro de carte d`identité",
               MessageType.Receiver,
               DateFormat("HH:mm").format(DateTime.now()));
+            
+              break;
+              case 'en_US':
+                 speak("Mr/Mrs $NomRec $PrenomRec send me your identity card number");
+            _insertSingleItem(
+                "Mr/Mrs $NomRec $PrenomRec send me your identity card number",
+                MessageType.Receiver,
+                DateFormat("HH:mm").format(DateTime.now()));
+              break;
+           }
+        
           v = 3;
         }
+         else{
+              switch (_currentLocaleId) {
+            case 'ar_SA':
+                    speak("قل الاسم الحقيقي من فضلك");
+          _insertSingleItem("قل الاسم الحقيقي من فضلك", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+               case 'fr_CA':
+                    speak("Dire un vrai prénom s`il vous plait");
+          _insertSingleItem("Dire un vrai prénom s`il vous plait", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+               case 'en_US':
+                     speak("Say a real name please");
+          _insertSingleItem("Say a real name please", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              
+              break;
+              }
+          }
         break;
       case 3:
-        print("la je suis cin " + txt);
         cinRec = txt;
         if ((cinRec.toString().length > 8)) {
-          speak("Génial maintenant c quoi votre adresse");
-          _insertSingleItem("Génial maintenant c quoi votre adresse",
+          switch(_currentLocaleId){
+          case 'ar_SA':
+              speak("عظيم الآن ما هو عنوانك");
+            _insertSingleItem(
+                " عظيم الآن ما هو عنوانك",
+                MessageType.Receiver,
+                DateFormat("HH:mm").format(DateTime.now()));
+            
+              break;
+              case 'fr_CA':
+        speak("Génial maintenant c`est quoi votre adresse");
+          _insertSingleItem("Génial maintenant c`est quoi votre adresse",
               MessageType.Receiver, DateFormat("HH:mm").format(DateTime.now()));
+              break;
+              case 'en_US':
+                 speak("Great now what is your address");
+            _insertSingleItem(
+                "Great now what is your address",
+                MessageType.Receiver,
+                DateFormat("HH:mm").format(DateTime.now()));
+              break;
+           }
+
           v = 4;
         }
+         else{
+              switch (_currentLocaleId) {
+            case 'ar_SA':
+                    speak("يجب أن يكون رقم بطاقة الهوية رقمًا أكبر من 8");
+          _insertSingleItem("يجب أن يكون رقم بطاقة الهوية رقمًا أكبر من 8", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+               case 'fr_CA':
+                    speak("Le numéro de carte d`identité doit etre un nombre supérieure à 8");
+          _insertSingleItem("Le numéro de carte d`identité doit etre un nombre sypérieure à 8", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+               case 'en_US':
+                    speak("The identity card number must be a number greater than 8");
+          _insertSingleItem("The identity card number must be a number greater than 8", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+              }
+          }
         break;
       case 4:
-        print("la je suis adresse " + txt);
         AdrRec = txt;
         if (AdrRec is String && NomRec.toString().trim().length>4) {
-          speak("Génial maintenant c quoi votre email");
-          _insertSingleItem("Génial maintenant c quoi votre email",
+          switch(_currentLocaleId){
+          case 'ar_SA':
+              speak("ماهو بريدك الإلكتروني");
+            _insertSingleItem(
+                "ماهو بريدك الإلكتروني",
+                MessageType.Receiver,
+                DateFormat("HH:mm").format(DateTime.now()));
+            
+              break;
+              case 'fr_CA':
+         speak("c`est quoi Votre email ");
+          _insertSingleItem("c`est quoi Votre email",
               MessageType.Receiver, DateFormat("HH:mm").format(DateTime.now()));
+              break;
+              case 'en_US':
+                 speak("what is your email");
+            _insertSingleItem(
+                "what is your email",
+                MessageType.Receiver,
+                DateFormat("HH:mm").format(DateTime.now()));
+              break;
+           }
+         
           v = 5;
         }
+         else{
+              switch (_currentLocaleId) {
+            case 'ar_SA':
+                    speak("هناك خطأ ما حاول مرة أخرى");
+          _insertSingleItem("هناك خطأ ما حاول مرة أخرى", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+               case 'fr_CA':
+                    speak("il y a une erreur réessayer");
+          _insertSingleItem("il y a une erreur réessayer", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+               case 'en_US':
+                    speak("there is an error try again");
+          _insertSingleItem("there is an error try again", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+              }
+          }
         break;
       case 5:
-        print("la je suis email " + txt);
         EmailRec = txt;
         if (RegExp(r'\S+@\S+\.\S+').hasMatch(EmailRec.toString())) {
-           speak("Choisir un type parmi cette liste dire 1 si la réclamation de type administration 2 si de type construction anarchique 3 si de type éclairage publique 4 si de type énergie 5 si de type espace verts 6 mobilité 7 santé et hiégiéne et 8 si c est une autre type");
+          switch(_currentLocaleId){
+          case 'ar_SA':
+              speak("اختر نوعًا من هذه القائمة قل 1 إذا كانت الشكوى من نوع الإدارة 2 إذا كانت من نوع البناء الفوضوي 3 إذا كانت من نوع الإضاءة العامة 4 إذا كانت من نوع الطاقة 5 إذا كانت من المساحة الخضراء اكتب 6 التنقل 7 الصحة والنظافة 8 إذا كان من نوع آخر");
+            _insertSingleItem(
+                "اختر نوعًا من هذه القائمة قل 1 إذا كانت الشكوى من نوع الإدارة 2 إذا كانت من نوع البناء الفوضوي 3 إذا كانت من نوع الإضاءة العامة 4 إذا كانت من نوع الطاقة 5 إذا كانت من المساحة الخضراء اكتب 6 التنقل 7 الصحة والنظافة 8 إذا كان من نوع آخر",
+                MessageType.Receiver,
+                DateFormat("HH:mm").format(DateTime.now()));
+            
+              break;
+              case 'fr_CA':
+       speak("Choisir un type parmi cette liste dire 1 si la réclamation de type administration 2 si de type construction anarchique 3 si de type éclairage publique 4 si de type énergie 5 si de type espace verts 6 mobilité 7 santé et hiégiéne et 8 si c est une autre type");
           _insertSingleItem("Choisir un type parmi cette liste dire 1 si la réclamation de type administration 2 si de type construction anarchique 3 si de type éclairage publique 4 si de type énergie 5 si de type espace verts 6 mobilité 7 santé et hiégiéne et 8 si c est un autre type", MessageType.Receiver,
               DateFormat("HH:mm").format(DateTime.now()));
+              break;
+              case 'en_US':
+                 speak("Choose a type from this list say 1 if the complaint is of the administration type 2 if of the anarchic construction type 3 if of the public lighting type 4 if of the energy type 5 if of the green space type 6 mobility 7 health and hygiene and 8 if it is another kind");
+            _insertSingleItem(
+                "Choose a type from this list say 1 if the complaint is of the administration type 2 if of the anarchic construction type 3 if of the public lighting type 4 if of the energy type 5 if of the green space type 6 mobility 7 health and hygiene and 8 if it is another kind",
+                MessageType.Receiver,
+                DateFormat("HH:mm").format(DateTime.now()));
+              break;
+           }
+           
         
           v = 6;
         } else {
-          speak("email non valide");
-          _insertSingleItem("email non valide", MessageType.Receiver,
+          switch (_currentLocaleId) {
+            case 'ar_SA':
+                    speak("البريد الإلكتروني غير صالح حاول مرة أخرى");
+          _insertSingleItem("البريد الإلكتروني غير صالح حاول مرة أخرى", MessageType.Receiver,
               DateFormat("HH:mm").format(DateTime.now()));
+              break;
+               case 'fr_CA':
+               speak("email non valide réessayer");
+          _insertSingleItem("email non valide réessayer", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+               case 'en_US':
+                    speak("invalid email try again");
+          _insertSingleItem("invalid email try again", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+            
+          }
+         
         }
         break;
           case 6:
-        print("la je suis type " + txt);
-        
         if (txt.toString().contains('1')) {
+
           type = "administration";
         }
         else  if (txt.toString().contains('2')) {
@@ -447,19 +631,50 @@ String? type;
          else  if (txt.toString().contains('8')) {
           type = "Autres Réclamations ";
         }
+        
+              switch (_currentLocaleId) {
+            case 'ar_SA':
+               speak("من فضلك أرسل لي وصفا موجزا لشكواك");
+          _insertSingleItem("من فضلك أرسل لي وصفا موجزا لشكواك", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));  
+              break;
+               case 'fr_CA':
           speak("Merci de m envoyer une petite description de votre réclamation");
           _insertSingleItem("Merci de m envoyer une petite description de votre réclamation", MessageType.Receiver,
-              DateFormat("HH:mm").format(DateTime.now()));
+              DateFormat("HH:mm").format(DateTime.now()));     
+              break;
+               case 'en_US':
+               speak("Please send me a short description of your complaint");
+          _insertSingleItem("Please send me a short description of your complaint", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));  
+              break;
+              }
+          
+         
           v = 7;
          
         break;
           case 7:
-        print("la je suis description " + txt);
         DescRec = txt;
         if (DescRec is String && DescRec.toString().trim().length>10){
-        speak("Demande enregistré");
-          _insertSingleItem("Demande enregistré", MessageType.Receiver,
+           
+              switch (_currentLocaleId) {
+            case 'ar_SA':
+               speak("تم تسجيل طلب الشكوى");
+          _insertSingleItem("تم تسجيل طلب الشكوى", MessageType.Receiver,
               DateFormat("HH:mm").format(DateTime.now()));
+              break;
+               case 'fr_CA':
+               speak("Demande de réclamation enregistré");
+          _insertSingleItem("Demande de réclamation enregistré", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+               case 'en_US':
+               speak("Complaint request registered");
+          _insertSingleItem("Complaint request registered", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+              }
           print("nom " +
               NomRec.toString() +
               " prenom " +
@@ -474,22 +689,41 @@ String? type;
               type.toString()
               + " description "+ DescRec.toString()); 
           
-          v = 8;
+          // v = 8;
         }
+         else{
+              switch (_currentLocaleId) {
+            case 'ar_SA':
+                 speak("قل وصفا صحيحا");
+          _insertSingleItem("قل وصفا صحيحا", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+               case 'fr_CA':
+                 speak("Dire une correcte description");
+          _insertSingleItem("Dire une correcte description", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+               case 'en_US':
+                 speak("Say a correct description");
+          _insertSingleItem("Say a correct description", MessageType.Receiver,
+              DateFormat("HH:mm").format(DateTime.now()));
+              break;
+              }
+          }
         break;
-      case 8:
-        print("demande enregisté");
-        print("nom " +
-            NomRec.toString() +
-            " prenom " +
-            PrenomRec.toString() +
-            " cin " +
-            cinRec.toString() +
-            // " email " +
-            // EmailRec.toString() +
-            " adr " +
-            AdrRec.toString());
-        break;
+      // case 8:
+      //   print("demande enregisté");
+      //   print("nom " +
+      //       NomRec.toString() +
+      //       " prenom " +
+      //       PrenomRec.toString() +
+      //       " cin " +
+      //       cinRec.toString() +
+      //       // " email " +
+      //       // EmailRec.toString() +
+      //       " adr " +
+      //       AdrRec.toString());
+      //   break;
     }
   }
  
